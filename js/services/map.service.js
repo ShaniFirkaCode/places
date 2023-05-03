faefeb464e185d4ede921cfec126ea0808b88478
Efrat Leibenzon <efrat.leibenzon@gmail.com>
2023-05-03 18:31:11 +0300

    map

--- js/services/map.service.js
+++ js/services/map.service.js
@@ -39,7 +39,7 @@ function panTo(lat, lng) {
 
 function _connectGoogleApi() {
     if (window.google) return Promise.resolve()
-    const API_KEY = '' //TODO: Enter your API Key
+    const API_KEY = 'AIzaSyC-vLni-u8_EjerVTVN8GpYSPqjXXN2u_A' //TODO: Enter your API Key
     var elGoogleApi = document.createElement('script')
     elGoogleApi.src = `https://maps.googleapis.com/maps/api/js?key=${API_KEY}`
     elGoogleApi.async = true
